c4d8a562e916ecebf7169d77cb09b0424fed49da
David de Hita <ddehita@alumni.unav.es>
2018-10-18 10:36:40 +0200

    minor changes

--- fasta_length.py
+++ fasta_length.py
@@ -1,22 +1,21 @@
 #!env python
 
-"""
-Python script to count the number of aminoacids per sequence in a FASTA file
+"""#Aniade un comentario al script
+Python script to count the number of aminoacidsor nucleotides per sequence in a FASTA file
 Call it like so:
     python fasta_length.py sequences.fasta
-More codding is needed
 """
 
 import sys
 
-sequence_length = 0
+sequence_length = 0 #an integer
 fasta_sequence_lengths = []
 
 # Getting the filename from the list of arguments
-fasta_filename = sys.argv[1]
+fasta_filename = sys.argv[1] #llama a la secuencia fasta
 
 # Opening the file:
-fastafile = open(fasta_filename, 'r')
+fastafile = open(fasta_filename, 'r') #Abre el archivo solo para leerlo 'r'
 
 # Iterating over all lines in the file:
 for line in fastafile.readlines():
@@ -25,10 +24,10 @@ for line in fastafile.readlines():
             fasta_sequence_lengths.append(sequence_length)
         sequence_length = 0
     else:
-        sequence_length += len(line.strip())
+        sequence_length += len(line.strip()) #+= es un shortcut que significa sequence_length + len(line.strip())
 fasta_sequence_lengths.append(sequence_length)
 
 # Closing the file:
-fastafile.close()
+fastafile.close() #Siempre se necesita cerrar el archivo despues de abrirlo con open
 
 print(sorted(fasta_sequence_lengths))
